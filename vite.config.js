0e159c8da13bb00472fa7daf60333b35180aff66
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-20 22:34:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true, // This makes the server accessible externally
-    hmr: {
-      host: 'siplus.smpplusbabussalam.my.id',
-      protocol: 'ws',
-    },
-    allowedHosts: ['siplus.smpplusbabussalam.my.id','ajwan.xyz'],
-  },
 })
